66abba58a59f32d7fcd699e9cf497b97bd798a6a
Sotam <4983166+Sotam@users.noreply.github.com>
2018-12-06 10:25:40 +0100

    Part 3 finished, what a mess.

--- AdventOfCode2018/Program.cs
+++ AdventOfCode2018/Program.cs
@@ -20,12 +20,19 @@ namespace AdventOfCode2018
             //var day1SolveB = day1.SolveB();
             //Console.WriteLine($"{nameof(Day1.SolveB)} is {day1SolveB} in {stopwatch.ElapsedMilliseconds} msec");
 
-            var day2 = new Day2();
+            //var day2 = new Day2();
             //var day2SolveA = day2.SolveA();
             //Console.WriteLine($"{nameof(Day2.SolveA)} is {day2SolveA} in {stopwatch.ElapsedMilliseconds} msec");
 
-            var day2SolveB = day2.SolveB();
-            Console.WriteLine($"{nameof(Day2.SolveB)} is {day2SolveB} in {stopwatch.ElapsedMilliseconds} msec");
+            //var day2SolveB = day2.SolveB();
+            //Console.WriteLine($"{nameof(Day2.SolveB)} is {day2SolveB} in {stopwatch.ElapsedMilliseconds} msec");
+
+            var day3 = new Day3();
+            var day3SolveA = day3.SolveA();
+            Console.WriteLine($"{nameof(Day3.SolveA)} is {day3SolveA} in {stopwatch.ElapsedMilliseconds} msec");
+
+            var day3SolveB = day3.SolveB();
+            Console.WriteLine($"{nameof(Day3.SolveB)} is {day3SolveB} in {stopwatch.ElapsedMilliseconds} msec");
 
             Console.ReadLine();
         }
